20c0438f58c4a5fc6a254ca7db9636db9937a9b6
Jonathan Dai <jonathanjddai@gmail.com>
2016-03-21 08:47:58 -0700

    update 3/21

--- js/script.js
+++ js/script.js
@@ -1,1 +1,83 @@
 console.log("hi")
+// AI'S START/SEQUENCE
+var randomSeq = []
+ function randomColor() {
+    var randomNumber = Math.random();
+    if (randomNumber < 0.25) {
+      return 'green'
+    } else if (randomNumber < 0.50) {
+      return 'red'
+    } else if (randomNumber < 0.75) {
+      return 'blue'
+    }
+      else {
+        return 'yellow'
+      }
+  }
+
+  function genSeq () {
+randomSeq.push(randomColor(),randomColor(),randomColor(),randomColor())}
+
+$('#start').click(function genSeq() {
+})
+
+
+
+
+// PLAYER TURN
+
+$('.squaresGreen').click(function() {
+setTimeout(function (){
+$('.squaresGreen').addClass('glowGreen')
+setTimeout(function() {
+$('.glowGreen').removeClass('glowGreen')
+}, 500)
+}, 500)
+});
+
+$('.squaresRed').click(function() {
+setTimeout(function (){
+$('.squaresRed').addClass('glowRed')
+setTimeout(function() {
+$('.glowRed').removeClass('glowRed')
+}, 500)
+}, 500)
+});
+
+$('.squaresBlue').click(function() {
+setTimeout(function (){
+$('.squaresBlue').addClass('glowBlue')
+setTimeout(function() {
+$('.glowBlue').removeClass('glowBlue')
+}, 500)
+}, 500)
+});
+
+$('.squaresYellow').click(function() {
+setTimeout(function (){
+$('.squaresYellow').addClass('glowYellow')
+setTimeout(function() {
+$('.glowYellow').removeClass('glowYellow')
+}, 500)
+}, 500)
+});
+
+// WIN LOGIC
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+

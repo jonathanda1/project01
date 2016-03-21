3c0f7ec25f3f9aaebf94e7b98d7b5fed36389d21
Jonathan Dai <jonathanjddai@gmail.com>
2016-03-21 09:58:45 -0700

    update2 3/21

--- js/script.js
+++ js/script.js
@@ -1,83 +1,68 @@
 console.log("hi")
-// AI'S START/SEQUENCE
-var randomSeq = []
- function randomColor() {
-    var randomNumber = Math.random();
-    if (randomNumber < 0.25) {
-      return 'green'
-    } else if (randomNumber < 0.50) {
-      return 'red'
-    } else if (randomNumber < 0.75) {
-      return 'blue'
-    }
-      else {
-        return 'yellow'
-      }
+
+function randomColor() {
+  var randomNumber = Math.random();
+  if (randomNumber < 0.25) {
+    return 'green';
+  } else if (randomNumber < 0.50) {
+    return 'red';
+  } else if (randomNumber < 0.75) {
+    return 'blue';
+  } else {
+    return 'yellow';
   }
+}
 
-  function genSeq () {
-randomSeq.push(randomColor(),randomColor(),randomColor(),randomColor())}
-
-$('#start').click(function genSeq() {
-})
+// $('play').click$(function genSeq())
 
 
+function generateRandomSequence(sequenceLength) {
+  var randomSequence = [];
 
+  // ...
+
+  return randomSequence;
+}
 
 // PLAYER TURN
 
+var playerSeq = [];
+
 $('.squaresGreen').click(function() {
-setTimeout(function (){
-$('.squaresGreen').addClass('glowGreen')
-setTimeout(function() {
-$('.glowGreen').removeClass('glowGreen')
-}, 500)
-}, 500)
+  setTimeout(function () {
+    $('.squaresGreen').addClass('glowGreen');
+    setTimeout(function() {
+      $('.glowGreen').removeClass('glowGreen');
+    }, 500);
+  }, 500);
 });
 
 $('.squaresRed').click(function() {
-setTimeout(function (){
-$('.squaresRed').addClass('glowRed')
-setTimeout(function() {
-$('.glowRed').removeClass('glowRed')
-}, 500)
-}, 500)
+  setTimeout(function () {
+    $('.squaresRed').addClass('glowRed');
+    setTimeout(function() {
+      $('.glowRed').removeClass('glowRed');
+    }, 500);
+  }, 500);
 });
 
 $('.squaresBlue').click(function() {
-setTimeout(function (){
-$('.squaresBlue').addClass('glowBlue')
-setTimeout(function() {
-$('.glowBlue').removeClass('glowBlue')
-}, 500)
-}, 500)
+  setTimeout(function () {
+    $('.squaresBlue').addClass('glowBlue');
+    setTimeout(function() {
+      $('.glowBlue').removeClass('glowBlue');
+      }, 500);
+  }, 500);
 });
 
 $('.squaresYellow').click(function() {
-setTimeout(function (){
-$('.squaresYellow').addClass('glowYellow')
-setTimeout(function() {
-$('.glowYellow').removeClass('glowYellow')
-}, 500)
-}, 500)
+  setTimeout(function () {
+    $('.squaresYellow').addClass('glowYellow');
+    setTimeout(function() {
+      $('.glowYellow').removeClass('glowYellow');
+    }, 500);
+  }, 500);
 });
 
 // WIN LOGIC
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+// playerSeq === randomSeq;
